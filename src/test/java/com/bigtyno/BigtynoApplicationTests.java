024bb07d6e3d8ee82cdeeac3be70b69dfdb4c358
bigtyno931128 <bigtyno4@gmail.com>
2023-07-13 22:34:47 +0900

    Initial commit

---
+++ src/test/java/com/bigtyno/BigtynoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigtyno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigtynoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
