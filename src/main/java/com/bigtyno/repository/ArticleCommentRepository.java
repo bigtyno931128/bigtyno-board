575ab86dbf3b74a38c1c5c8e90123039c17d4567
bigtyno931128 <bigtyno4@gmail.com>
2023-07-17 21:32:14 +0900

    #3 - DB
    테이블과 객체를 매핑하기 위한 JPA REPO 생성

--- src/main/java/com/bigtyno/repository/ArticleCommentRepository.java
+++ src/main/java/com/bigtyno/repository/ArticleCommentRepository.java
@@ -1,4 +1,9 @@
 package com.bigtyno.repository;
 
-public interface ArticleCommentRepository {
+import com.bigtyno.domain.entity.ArticleCommentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleCommentRepository extends JpaRepository<Long, ArticleCommentEntity> {
 }
